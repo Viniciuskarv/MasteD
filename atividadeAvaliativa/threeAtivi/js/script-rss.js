db9e6ccc807abb4182534fe3d302cb39ccf4e6d4
Viniciuskarv <104027764+Viniciuskarv@users.noreply.github.com>
2023-02-24 04:06:16 +0000

    add

--- atividadeAvaliativa/threeAtivi/js/script-rss.js
+++ atividadeAvaliativa/threeAtivi/js/script-rss.js
@@ -7,12 +7,12 @@ window.onload = function() {
             // ler o conteúdo
             var frase = "";
             for (i = 0; i < objeto_json.items.length; i++){
-                frase += '<div class="card mb-2 shadow p-3 mb-3 bg-body rounded " style="max-width: 95%;">'
+                frase += '<div class="card mb-2 p-3 mb-3 bg-body rounded " style="max-width: 100%;">'
                 frase += '<div class="row">'
-                frase += '<div class="col-xxl-4">'
+                frase += '<div class="col-md-12 col-sm-4">'
                 frase += '<img src="'+ objeto_json.items[i].enclosure.link +'" class="img-fluid rounded-start" alt="imgNoticia">'
                 frase += '</div>'
-                frase += '<div class="col-xl-8">'
+                frase += '<div class="col-md-12 col-sm-8">'
                 frase += '<div class="card-body">'
                 frase += '<a class="card-title" href="' + objeto_json.items[i].link + '">'+ objeto_json.items[i].title +'</a>'
                 frase += '<p>' + objeto_json.items[i].pubDate + '</p>'
@@ -24,28 +24,31 @@ window.onload = function() {
             $("#caixa").html(frase);
 
             var car1 = '<img src="'+ objeto_json.items[1].enclosure.link +'" class="img-fluid rounded" alt="imgNoticia">'
-            car1 += '<div class="carousel-caption d-none d-md-block">'
-            car1 += '<p>'+ objeto_json.items[1].title + '</p>'
+            car1 += '<div class="carousel-caption">'
+            car1 += '<a class="card-title" href="'+ objeto_json.items[1].link +'">'+ objeto_json.items[1].title + '</a>'
             car1 += '</div>'
             
             $("#Carousel0").html(car1);
 
             var car2 = '<img src="'+ objeto_json.items[2].enclosure.link +'" class="img-fluid rounded" alt="imgNoticia">'
-            car2 += '<div class="carousel-caption d-none d-md-block">' 
-            car2 += '<p>'+ objeto_json.items[2].title + '</p>'
+            car2 += '<div class="carousel-caption">' 
+            car2 += '<a class="card-title" href="'+ objeto_json.items[2].link +'">'+ objeto_json.items[2].title + '</a>'
             car2 += '</div>'
            
             $("#Carousel1").html(car2);
 
             var car3 = '<img src="'+ objeto_json.items[3].enclosure.link +'" class="img-fluid rounded" alt="imgNoticia">'
-            car3 += '<div class="carousel-caption d-none d-md-block">'
-            car3 += '<p>'+ objeto_json.items[3].title + '</p>'
+            car3 += '<div class="carousel-caption">'
+            car3 += '<a class="card-title" href="'+ objeto_json.items[3].link +'">'+ objeto_json.items[3].title + '</a>'
             car3 += '</div>'
             
             $("#Carousel2").html(car3);
         }
     });
 }
+$(document).ready(function() {
+    $('.image-link').magnificPopup({type:'image'});
+  });
 // Alert
 //$(document).ready(function(){
 //    setTimeout(function(){
